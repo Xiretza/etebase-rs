a25241eb3f693a5f9bee4740d17e930c5f5c502c
Xiretza <xiretza@xiretza.xyz>
2022-05-25 23:35:35 +0200

    feat(errors): better Display impl for Error

--- src/error.rs
+++ src/error.rs
@@ -2,7 +2,6 @@
 // SPDX-License-Identifier: LGPL-2.1-only
 
 use std::error::Error as StdError;
-use std::fmt;
 use thiserror::Error;
 
 use crate::http_client::ErrorResponse;
@@ -14,66 +13,42 @@ pub type Result<T> = std::result::Result<T, Error>;
 #[derive(Debug, Error)]
 #[non_exhaustive]
 pub enum Error {
-    /// An IO error
+    #[error("IO error")]
     Io(#[from] std::io::Error),
 
-    /// An error with parsing the a URL (e.g. from the server URL)
+    #[error("URL parsing failed")]
     UrlParse(#[from] url::ParseError),
 
-    /// An error related to msgpack serialization
+    #[error("MessagePack serialization failed")]
     MsgPackEncode(#[from] rmp_serde::encode::Error),
-    /// An error related to msgpack deserialization
+    #[error("MessagePack deserialization failed")]
     MsgPackDecode(#[from] rmp_serde::decode::Error),
 
-    /// A programming error that indicates the developers are using the API wrong
+    #[error("library usage error: {0}")]
     ProgrammingError(&'static str),
-    /// An attempt to fetch the content of an item that doesn't have the content yet
+    #[error("attempted to fetch content of an item that doesn't have the content yet: {0}")]
     MissingContent(&'static str),
-    /// An issue with the padding of the encrypted content
+    #[error("issue with the padding of the encrypted content: {0}")]
     Padding(&'static str),
-    /// An issue with the Base64 decoding
+    #[error("base64 encoding/decoding failed: {0}")]
     Base64(&'static str),
-    /// An issue with the encryption
+    #[error("encryption error: {0}")]
     Encryption(&'static str),
-    /// An authorization issue from the server
+    #[error("authorization error: {0}")]
     Unauthorized(ErrorResponse),
-    /// A conflict issue returned from the server, e.g. if a transaction failed
+    #[error("data conflict: {0}")]
     Conflict(String),
-    /// The operation was not allowed due to permissions
+    #[error("permission denied: {0}")]
     PermissionDenied(ErrorResponse),
 
-    /// A generic error with the server request
+    #[error("HTTP request failed: status {status}, {response}")]
     Http {
         status: u16,
         response: ErrorResponse,
     },
 
-    /// A network error from within the HTTP library
+    #[error("network error")]
     Network(#[source] Box<dyn StdError>),
-}
-
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        #[allow(clippy::match_same_arms)] // same order as in type declaration
-        match self {
-            Error::Io(s) => s.fmt(f),
-            Error::UrlParse(s) => s.fmt(f),
-            Error::MsgPackEncode(s) => s.fmt(f),
-            Error::MsgPackDecode(s) => s.fmt(f),
-            Error::ProgrammingError(s) => s.fmt(f),
-            Error::MissingContent(s) => s.fmt(f),
-            Error::Padding(s) => s.fmt(f),
-            Error::Base64(s) => s.fmt(f),
-            Error::Encryption(s) => s.fmt(f),
-            Error::PermissionDenied(s) => s.fmt(f),
-            Error::Unauthorized(s) => s.fmt(f),
-            Error::Conflict(s) => s.fmt(f),
-
-            Error::Http { status, response } => write!(f, "HTTP status {}: {}", status, response),
-
-            Error::Network(s) => s.fmt(f),
-        }
-    }
 }
 
 impl From<Error> for String {
