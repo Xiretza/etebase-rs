93e3c685175a7316d8c4a19e049cb8b2f3ed0bac
Xiretza <xiretza@xiretza.xyz>
2022-06-22 13:02:22 +0200

    fix(lints): silence clippy::similar_names lints

--- src/online_managers.rs
+++ src/online_managers.rs
@@ -314,11 +314,9 @@ impl<'a> Authenticator<'a> {
         let body = rmp_serde::to_vec_named(&body_struct)?;
 
         let url = self.api_base.join("login_challenge/")?;
-        let res = self.client.post(url.as_str(), body)?;
-        res.error_for_status()?;
-        let res = res.bytes();
-
-        let ret: LoginChallange = rmp_serde::from_slice(res)?;
+        let ret = self.client.post(url.as_str(), body)?;
+        ret.error_for_status()?;
+        let ret = rmp_serde::from_slice(ret.bytes())?;
 
         Ok(ret)
     }
@@ -341,11 +339,9 @@ impl<'a> Authenticator<'a> {
         let body = rmp_serde::to_vec_named(&body_struct)?;
 
         let url = self.api_base.join("signup/")?;
-        let res = self.client.post(url.as_str(), body)?;
-        res.error_for_status()?;
-        let res = res.bytes();
-
-        let ret: LoginResponse = rmp_serde::from_slice(res)?;
+        let ret = self.client.post(url.as_str(), body)?;
+        ret.error_for_status()?;
+        let ret = rmp_serde::from_slice(ret.bytes())?;
 
         Ok(ret)
     }
@@ -358,11 +354,9 @@ impl<'a> Authenticator<'a> {
         let body = rmp_serde::to_vec_named(&body_struct)?;
 
         let url = self.api_base.join("login/")?;
-        let res = self.client.post(url.as_str(), body)?;
-        res.error_for_status()?;
-        let res = res.bytes();
-
-        let ret: LoginResponse = rmp_serde::from_slice(res)?;
+        let ret = self.client.post(url.as_str(), body)?;
+        ret.error_for_status()?;
+        let ret = rmp_serde::from_slice(ret.bytes())?;
 
         Ok(ret)
     }
@@ -396,11 +390,9 @@ impl<'a> Authenticator<'a> {
         }
 
         let url = self.api_base.join("dashboard_url/")?;
-        let res = self.client.post(url.as_str(), vec![])?;
-        res.error_for_status()?;
-        let res = res.bytes();
-
-        let ret: Ret = rmp_serde::from_slice(res)?;
+        let ret = self.client.post(url.as_str(), vec![])?;
+        ret.error_for_status()?;
+        let ret: Ret = rmp_serde::from_slice(ret.bytes())?;
 
         Ok(ret.url)
     }
